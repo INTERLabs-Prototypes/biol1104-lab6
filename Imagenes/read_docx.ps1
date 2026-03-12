36d05d64fce37dfbb5de5911ecc39fadbc097cd3
INTERLabs-Prototypes <159587012+INTERLabs-Prototypes@users.noreply.github.com>
2026-03-12 19:07:44 -0400

    Delete Imagenes/read_docx.ps1

--- Imagenes/read_docx.ps1
+++
@@ -1,7 +0,0 @@
-$word = New-Object -ComObject Word.Application
-$word.Visible = $false
-$doc = $word.Documents.Open("d:\Documents\Personal\Arlene\Inter\BIOL 1104\BIOL 1104 Labs\Lab #6\01_BIOL1104_HojaEN-Lab6.docx")
-$text = $doc.Content.Text
-$text | Out-File -Encoding utf8 "d:\Documents\Personal\Arlene\Inter\BIOL 1104\BIOL 1104 Labs\Lab #6\lab6_content.txt"
-$doc.Close($false)
-$word.Quit()
